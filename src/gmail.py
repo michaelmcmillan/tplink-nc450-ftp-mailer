0f2b17bd73a0af03ebbab37a10f32e50eac5aabe
Michael McMillan <email@michaelmcmillan.net>
2017-08-13 23:02:21 +0200

    reinstantiate the smtp connection

--- src/gmail.py
+++ src/gmail.py
@@ -8,18 +8,15 @@ class Gmail:
 
     def __init__(self):
         self.credentials = (Configuration.email_username, Configuration.email_password)
-        self.connection = SMTP('smtp.gmail.com', 587)
 
     def connect(self):
+        self.connection = SMTP('smtp.gmail.com', 587)
         self.connection.ehlo()
         self.connection.starttls()
         self.connection.login(*self.credentials)
 
     def disconnect(self):
-        try:
-            self.connection.close()
-        except:
-            pass
+        self.connection.close()
 
     def send(self, message):
         raw_message = MIMEMultipart()
